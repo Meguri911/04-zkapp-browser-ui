deef02e783b0ffe37b4ab9ec87a8442e019a65b8
Meguri <ekoyud99@gmail.com>
2022-12-14 08:43:27 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-e903f24ef6199714.js
+++ _next/static/chunks/webpack-e903f24ef6199714.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "187159d471a8f634" + ".css";
+/******/ 			return "static/css/" + "56346e520ce6e7fd" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
